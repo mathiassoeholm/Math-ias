9ffa20f2856b758305b1e2be7f6bc7ff98439f22
Mathias Soeholm <mathiassoeholm@gmail.com>
2015-12-29 22:04:48 +0100

    Implemented slerp function

--- src/quaternion.h
+++ src/quaternion.h
@@ -5,6 +5,7 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include "Matrix4x4.h"
+#include <limits>
 
 namespace Math_ias
 {
@@ -17,7 +18,7 @@ namespace Math_ias
 		{
 			T sqrLength = _q[0]*_q[0] + _q[1]*_q[1] + _q[2]*_q[2] + _q[3]*_q[3];
 
-			if(abs(1 - sqrLength) < 0.00001)
+			if(abs(1 - sqrLength) < std::numeric_limits<T>().epsilon())
 			{
 				return;
 			}
@@ -42,12 +43,12 @@ namespace Math_ias
 			_q[1] = sin(radians / 2)*x;
 			_q[2] = sin(radians / 2)*y;
 			_q[3] = sin(radians / 2)*z;
+
+			renormalize();
 		}
 
-		Matrix4x4<T> getMatrix()
+		Matrix4x4<T> getMatrix() const
 		{
-			renormalize();
-
 			return Matrix4x4<T>(
 				1 - 2*_q[2]*_q[2] - 2*_q[3]*_q[3], 2*_q[1]*_q[2] - 2*_q[0]*_q[3], 2*_q[1]*_q[3] + 2*_q[0]*_q[2], 0,
 				2*_q[1]*_q[2] + 2*_q[0]*_q[3], 1 - 2*_q[1]*_q[1] - 2*_q[3]*_q[3], 2*_q[2]*_q[3] - 2*_q[0]*_q[1], 0,
@@ -73,6 +74,44 @@ namespace Math_ias
 			result._q[2] = other._q[0] * _q[2] + other._q[1] * _q[3] + other._q[2] * _q[0] - other._q[3] * _q[1];
 			result._q[3] = other._q[0] * _q[3] - other._q[1] * _q[2] + other._q[2] * _q[1] + other._q[3] * _q[0];
 			
+			result.renormalize();
+
+			return result;
+		}
+
+		static Quaternion<T> slerp(const Quaternion<T>& start, const Quaternion<T>& end, T t)
+		{
+			T dot = start._q[0] * end._q[0] + start._q[1] * end._q[1] + start._q[2] * end._q[2] + start._q[3] * end._q[3];
+
+			if(abs(dot) > 1.0 - std::numeric_limits<T>().epsilon())
+			{
+				return start;
+			}
+
+			T sinOmega = sqrt(1 - dot * dot);
+
+			Quaternion<T> result;
+
+			// If angle is 180, do linear interpolation
+			if(abs(sinOmega) < std::numeric_limits<T>().epsilon())
+			{
+				for (auto i = 0; i < 4; i++)
+				{
+					result._q[i] = (1 - t) * start._q[i] + t * end._q[i];
+				}
+
+				return result;
+			}
+
+			T omega = acos(dot);
+			T a = sin((1 - t) * omega) / sinOmega;
+			T b = sin(t*omega) / sinOmega;
+
+			for (auto i = 0; i < 4; i++)
+			{
+				result._q[i] = start._q[i] * a + end._q[i] * b;
+			}
+
 			return result;
 		}
     };
